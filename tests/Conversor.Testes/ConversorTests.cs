4054e848ad1bbd9e2f56d33c87e18df663b220cb
Matheus-Morais <matheus2992morais@gmail.com>
2023-05-12 11:35:20 -0300

    Adiciona Teste InteiroParaRomano

--- tests/Conversor.Testes/ConversorTests.cs
+++ tests/Conversor.Testes/ConversorTests.cs
@@ -1,10 +1,29 @@
 namespace Conversor.Testes;
 
+using ConversorAlgarismosRomanos;
+
 public class ConversorTests
 {
-    [Fact]
-    public void Test1()
-    {
 
+    /*
+     * Tabela de Conversão Romano-Inteiro
+        I - 1
+        II - 2
+        III - 3
+        IV - 4
+        V - 5
+     */
+
+    Conversor conversor = new Conversor();
+
+    [Fact]
+    public void TesteConversorInteiroParaRomano()
+    {
+        int a = 10;
+        string esperado = "X";
+
+        string algarismo = conversor.InteiroParaRomano(a);
+
+        Assert.Equal(esperado, algarismo);
     }
 }
