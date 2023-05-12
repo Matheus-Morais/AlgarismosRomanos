a72a929aea6a6e128664928967d6782ed7de71f8
Matheus-Morais <matheus2992morais@gmail.com>
2023-05-12 11:33:42 -0300

    Conversor Base

--- src/ConversorAlgarismosRomanos/Conversor.cs
+++ src/ConversorAlgarismosRomanos/Conversor.cs
@@ -1,5 +1,19 @@
 ﻿namespace ConversorAlgarismosRomanos;
-public class Conversor
+public class Conversor : ConversorRomano
 {
+    public string InteiroParaRomano(int numero)
+    {
+        throw new NotImplementedException();
+    }
 
+    public int RomanoParaInteiro(string romano)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface ConversorRomano
+{
+    string InteiroParaRomano(int numero);
+    int RomanoParaInteiro(string romano);
 }
